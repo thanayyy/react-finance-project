957924b7cca8386025a3829a5c83c000834c584d
thanayut <thanayyy5998@gmail.com>
2026-01-18 23:30:28 +0700

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
 import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 function App() {
   return (
       <div>
         <Navbar />
+        <Hero />
       </div>
   )
 }
